d8a7b5bae6d8b48d3c6623a8fbf1078592375b8f
lennyb94000 <124142597+lennyb94000@users.noreply.github.com>
2023-02-13 18:05:26 +0100

    Delete Site directory

--- Site/bouton.js
+++
@@ -1,8 +0,0 @@
-const boxes = document.querySelectorAll('.boxo');
-
-boxes.forEach(function(boxo) {
-  boxo.addEventListener('click', function() {
-    // code à exécuter lorsque l'utilisateur clique sur le bouton
-    console.log('Bouton cliqué !');
-  });
-});
